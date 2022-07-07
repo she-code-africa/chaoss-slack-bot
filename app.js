18a33e13bdb53ca4f3beaec22d85f207911e52ca
mide358 <iyimide.adegunloye@gmail.com>
2022-07-07 18:41:38 +0100

    edited welcome message
    Signed-off-by: mide358 <iyimide.adegunloye@gmail.com>

--- app.js
+++ app.js
@@ -18,6 +18,41 @@ app.message(/hello|hey|hi/i, async ({ message, say }) => {
   await say(`Hello there <@${message.user}>!`);
 });
 
+app.message(/test|testing/i, async ({ message, say }) => {
+  await say({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey <@${event.user.id}>,\n Welcome to the *She Code Africa Community* !🎉\n She Code Africa is a non-profit organization aimed at celebrating, and empowering women in technology in Africa. With over 15,000 women in this community, and several chapters spread across Africa, our aim is to provide a safe space where women like you can network, learn and grow in your respective tech fields.
+          `,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Here are the things you need to do to fully confirm your membership:\n
+               • Update your profile with a picture of you and what you do, so as to help others put a face to your name and prove your authenticity in the community. \n
+               • Make an intro of yourself in the #introductions channel (with format stated on guideline) so others can meet you  and possibly connect.\n
+               • Join any of these technical channels in order to learn and grow with other women in your field
+                  \t\t ❇️ <#C03N8QPL7FZ>\n
+                  \t\t ❇️ <#C03N8QPL7FZ>\n
+                  \t\t ❇️ <#C03N8QPL7FZ>\n
+            \n \n
+            Take a minute to go through our community guidelines, here, to better understand how to access each learning channel and our rules against spam posts/cross posting/irrelevant posts. Feel free to reach out to the community managers, <#D03961HLBTN>  or <@amarachukwu> if you have any questions.
+
+
+
+          `,
+        },
+      },
+    ],
+    text: `Welcome to the team, <@${message.user}>! 🎉 You can introduce yourself in this channel.`,
+  });
+});
+
 // app.message(/newbie/i, async ({ client, message, logger }) => {
 //   try {
 //     const result = await client.chat.postMessage({
@@ -586,291 +621,52 @@ app.message(/hello|hey|hi/i, async ({ message, say }) => {
 
 // //****************************************** */
 
-// // When a user joins the team, the bot sends a DM to the newcommer asking them how they would like to contribute
-// app.event('team_join', async ({ event, client, logger }) => {
-//   try {
-//     const result = await client.chat.postMessage({
-//       channel: event.user.id,
-//       blocks: [
-//         {
-//           type: 'section',
-//           text: {
-//             type: 'mrkdwn',
-//             text: `Welcome to *CHAOSS Community* <@${event.user.id}>! 🎉How would you like to get started? \n\nI want to..`,
-//           },
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Develop Metrics',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'develop',
-//               action_id: 'develop',
-//             },
-//           ],
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Join a Meeting',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'joinMeet',
-//               action_id: 'joinMeet',
-//             },
-//           ],
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Contribute or Review Code',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'contribute',
-//               action_id: 'contribute',
-//             },
-//           ],
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Help with the Website',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'website',
-//               action_id: 'helpWithWebsite',
-//             },
-//           ],
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Write or Edit Documentation',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'docs',
-//               action_id: 'docs',
-//             },
-//           ],
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Contribute through a Mentorship Program',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'mentorship',
-//               action_id: 'mentorship',
-//             },
-//           ],
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Implement Metrics in my Project',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'implement_metrics',
-//               action_id: 'implement_metrics',
-//             },
-//           ],
-//         },
-//         {
-//           type: 'actions',
-//           elements: [
-//             {
-//               type: 'button',
-//               text: {
-//                 type: 'plain_text',
-//                 text: 'Learn About Something Else',
-//                 emoji: true,
-//               },
-//               style: 'primary',
-//               value: 'learn_something_else',
-//               action_id: 'learn_something_else',
-//             },
-//           ],
-//         },
-//       ],
-//       text: `Welcome to the team, <@${event.user.id}>! 🎉.`,
-//     });
-//     logger.info(result);
-//   } catch (error) {
-//     logger.error(error);
-//   }
-// });
+//  When a user joins the team, the bot sends a DM to the newcommer asking them how they would like to contribute
+/*
+app.event('team_join', async ({ event, client, logger }) => {
+  try {
+    const result = await client.chat.postMessage({
+      channel: event.user.id,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Hey <@${event.user.id}>,\n Welcome to the *She Code Africa Community* !🎉\n She Code Africa is a non-profit organization aimed at celebrating, and empowering women in technology in Africa. With over 15,000 women in this community, and several chapters spread across Africa, our aim is to provide a safe space where women like you can network, learn and grow in your respective tech fields.
+            `,
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Here are the things you need to do to fully confirm your membership:\n
+                 • Update your profile with a picture of you and what you do, so as to help others put a face to your name and prove your authenticity in the community. \n
+                 • Make an intro of yourself in the #introductions channel (with format stated on guideline) so others can meet you  and possibly connect.\n
+                 • Join any of these technical channels in order to learn and grow with other women in your field
+                    \t\t ❇️ <#C03N8QPL7FZ>\n
+                    \t\t ❇️ <#C03N8QPL7FZ>\n
+                    \t\t ❇️ <#C03N8QPL7FZ>\n
+              \n \n
+              Take a minute to go through our community guidelines, here, to better understand how to access each learning channel and our rules against spam posts/cross posting/irrelevant posts. Feel free to reach out to the community managers, <#D03961HLBTN>  or <@amarachukwu> if you have any questions.
 
-// // handle the button click and show the responses
-// app.action('develop', async ({ ack, say }) => {
-//   await ack();
-//   await say(
-//     `You clicked *Develop Metrics*\n
-//     There are 5 Working Groups that develop metrics based on different aspects of open source community health: Risk, Value, Evolution, DEI, and Common.  More information about each of these groups can be found here: <https://handbook.chaoss.community/community-handbook/community-initiatives/working-groups> and the metrics are developed during our Working Group meetings.
-//     `
-//   );
-// });
 
-// app.action('joinMeet', async ({ ack, say }) => {
-//   // Acknowledge the action
-//   await ack();
-//   await say(
-//     `You clicked *Join Meeting*\n
-//     All CHAOSS meetings are open to everyone, and they happen virtually at  <https://zoom.us/my/chaoss> We recommend a good first meeting is our Weekly Community Call (Every Tuesday at 11:00 am US Central/Chicago time) but you can see a calendar of all our meetings at https://chaoss.community/participate.
-//     `
-//   );
-// });
 
-// app.action('contribute', async ({ ack, say }) => {
-//   // Acknowledge the action
-//   await ack();
-//   await say(
-//     `You clicked *Contribute or Review code*\n
-//     You are welcome to contribute. Please see this documentation: https://handbook.chaoss.community/community-handbook/contributing/development.
-//     `
-//   );
-// });
+            `,
+          },
+        },
+      ],
+      text: `Welcome to the team, <@${event.user.id}>! 🎉.`,
+    });
+    logger.info(result);
+  } catch (error) {
+    logger.error(error);
+  }
+});
+*/
+// handle the button click and show the responses
 
-// app.action('helpWithWebsite', async ({ ack, say }) => {
-//   await ack();
-//   await say(
-//     `You clicked *Help with the Website*\n
-//     The first step is getting to know our community! You can connect with us in any of the ways described in our Participate page here: https://chaoss.community/participate.
-//     `
-//   );
-// });
-
-// app.action('docs', async ({ ack, say }) => {
-//   await ack();
-//   await say(
-//     `
-//     You clicked *Write or Edit Documentation*\n
-//     Please see this documentation: https://handbook.chaoss.community/community-handbook/contributing/documentation.
-//     `
-//   );
-// });
-
-// app.action('mentorship', async ({ ack, say }) => {
-//   await ack();
-//   await say({
-//     // Choosing "Contribute through a mentorship program" triggers these nested radio buttons
-//     blocks: [
-//       {
-//         type: 'section',
-//         text: {
-//           type: 'mrkdwn',
-//           text: 'Which mentorship program are you interested in?',
-//         },
-//         accessory: {
-//           type: 'radio_buttons',
-//           options: [
-//             {
-//               text: {
-//                 type: 'plain_text',
-//                 text: '*Outreachy*',
-//                 emoji: true,
-//               },
-//               value: 'outreachy',
-//             },
-//             {
-//               text: {
-//                 type: 'plain_text',
-//                 text: '*Google Summer of Code*',
-//                 emoji: true,
-//               },
-//               value: 'gsoc',
-//             },
-//             {
-//               text: {
-//                 type: 'plain_text',
-//                 text: '*Google Season of Docs*',
-//                 emoji: true,
-//               },
-//               value: 'gsod',
-//             },
-//           ],
-//           action_id: 'mentorship_selection',
-//         },
-//       },
-//     ],
-//     text: 'Which mentorship program are you interested in?',
-//   });
-// });
-
-// // this handler is for the nested radio buttons above
-// app.action('mentorship_selection', async ({ action, ack, say }) => {
-//   await ack();
-//   console.log(action.selected_option.value);
-//   if (action.selected_option.value === 'outreachy') {
-//     await say(
-//       `You clicked *Outreachy*\n
-//       The Outreachy Application period has ended for our participation in this program this year. You can still join the CHAOSS community in any of the ways found on our Participate page here: https://chaoss.community/participate.`
-//     );
-//   }
-//   if (action.selected_option.value === 'gsoc') {
-//     await say(
-//       `You clicked *Google Summer of Code*\n
-//       The Google Summer of Code Application period has ended for our participation in this program this year. You can still join the CHAOSS community in any of the ways found on our Participate page here: https://chaoss.community/participate.`
-//     );
-//   }
-//   if (action.selected_option.value === 'gsod') {
-//     await say(
-//       `You clicked *Google Season of Docs*\n
-//       Welcome! If you haven't yet, please join the <#C03C239HN1F> Slack channel and take note of the pinned item at the top of the channel for more information about next steps.`
-//     );
-//   }
-// });
-
-// app.action('implement_metrics', async ({ ack, say }) => {
-//   await ack();
-//   await say(
-//     `You clicked *Implement Metrics*\n
-//     We encourage you to join one of our Working Groups for specific questions about implementing your metrics. You can read more about them here: https://handbook.chaoss.community/community-handbook/community-initiatives/working-groups.`
-//   );
-// });
-
-// app.action('learn_something_else', async ({ ack, say }) => {
-//   await ack();
-//   await say(
-//     `You clicked *Learn About Something Else*\n
-//     We encourage you to read through our Community Handbook: https://handbook.chaoss.community/community-handbook/, and if you still can't find what you're looking for, feel free to ask your question in our #newcomers slack channel.`
-//   );
-// });
+// this handler is for the nested radio buttons above
 
 // //*************************************************************** */
 
